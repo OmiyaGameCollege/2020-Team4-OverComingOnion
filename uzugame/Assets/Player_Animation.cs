d21a7b9c1358cbb2a405a039ef47fb5789a2dbbf
kobayashitakumu <ezweb.ne.jp1998@gmail.com>
2020-10-19 00:51:56 +0900

    キャラクターのアニメーション

---
+++ uzugame/Assets/Player_Animation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Animation : MonoBehaviour
+{
+    private Animator anim = null;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        if (horizontalKey > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("run", true);
+        }
+        else if (horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("run", true);
+        }
+        else
+        {
+            anim.SetBool("run", false);
+        }
+    }
+}
