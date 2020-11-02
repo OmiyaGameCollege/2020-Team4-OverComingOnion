1618f6d22711daecf45556060beea6b3bcbcf170
kobayashitakumu <ezweb.ne.jp1998@gmail.com>
2020-11-02 14:14:40 +0900

    SceneChange part3!

---
+++ Assets/NextStage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;  //「2」
+
+public class NextStage : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("BOSS");   
+        }
+    }
+}
