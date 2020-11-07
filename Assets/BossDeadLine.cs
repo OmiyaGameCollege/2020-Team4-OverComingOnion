1e033ceb084460b372d932b0712c9958909fd1da
kobayashitakumu <ezweb.ne.jp1998@gmail.com>
2020-11-07 23:22:24 +0900

    BossStage

---
+++ Assets/BossDeadLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BossDeadLine : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "BOSS")
+            SceneManager.LoadScene("BossGameOver");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/BossGameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;  //「2」
+
+public class BossGameOver : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title");    //「3」
+        }
+
+    }
+}
