f6e8c7204acd9ab31b8d80efce217caacbb45ff7
yuki7854koba741 <yuki7854koba741@gmail.com>
2020-11-30 14:44:41 +0900

    animation

--- Assets/GOAL.cs
+++ Assets/GOAL.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GOAL : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         SceneManager.LoadScene("GameClear");
     }
